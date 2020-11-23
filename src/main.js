51195d5664475b7ff7ab91ed321506bbe097c5fc
FX Morel <efix.morel@gmail.com>
2020-11-23 17:40:32 +0100

    Try to include alphavantage

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+var obj = {};
+obj.alpha = alphavantage({ key: 'L7B1PRR8D9I68OSZ' });
+obj.alpha.data.daily(`ibm`).then((data) => {
+  sonsole.log(data);
+  obj.metadata = data["Meta Data"].toString();
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
+        message: obj.metadata
+    },
 });
