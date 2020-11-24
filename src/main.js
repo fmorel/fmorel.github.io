85a4e13ea4ea94cd57bab8111d61b3a039565be6
FX Morel <efix.morel@gmail.com>
2020-11-24 15:10:28 +0100

    Alphavantage works !

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-var obj = {};
-obj.alpha = alphavantage({ key: 'L7B1PRR8D9I68OSZ' });
-obj.alpha.data.daily(`ibm`).then((data) => {
-  sonsole.log(data);
-  obj.metadata = data["Meta Data"].toString();
-});
-
 var app = new Vue({
     el: '#app',
     data: {
-        message: obj.metadata
+        message: "waiting ...",
+        date: ""
     },
+    created : function () {
+        var fullDate = new Date();
+        this.date = fullDate.toDateString();
+    },
+    mounted : function () {
+        var alpha = alphavantage({ key: 'L7B1PRR8D9I68OSZ' });
+        alpha.data.daily('SQNS').then((data) => {
+            var daily = data["Time Series (Daily)"];
+            this.date = Object.keys(daily)[0];
+            var firstDay = daily[this.date];
+            this.message = "Open @" + firstDay["1. open"] + ". Close @" + firstDay["4. close"];
+        });
+    }
 });
