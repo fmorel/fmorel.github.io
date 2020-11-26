ce46697e0919892612d350570aed9c449a1c09ad
FX Morel <efix.morel@gmail.com>
2020-11-26 12:11:08 +0100

    Add a component :)

--- src/main.js
+++ src/main.js
@@ -1,20 +1,60 @@
+
+Vue.component('portfolio-element', {
+    props: {
+        stock: Object
+    },
+    template: `
+        <tr>
+        <td> {{stock.symbol}} </td>
+        <td> {{stock.buying_price}} </td>
+        <td style="color:green"> {{stock.price}} </td>
+        <td> <button v-on:click="$emit('sell-stock')"> Sell </button> </td>
+        </tr>
+    `
+});
+
+
+var alpha_wrapper = {
+    alpha : alphavantage({ key: 'L7B1PRR8D9I68OSZ' }),
+
+    fetch_price : function (stock) {
+        this.alpha.data.daily(stock.symbol).then((data) => {
+            const daily = data["Time Series (Daily)"];
+            const date = Object.keys(daily)[0];
+            const firstDay = daily[date];
+            stock.price = firstDay["1. open"];
+        });
+    }
+}
+            
+
 var app = new Vue({
     el: '#app',
     data: {
         message: "waiting ...",
-        date: ""
+        stocks: [
+          {
+            id: 0,
+            symbol: 'SQNS',
+            buying_price: 2.00,
+            price: '...'
+          },
+          {
+            id: 1,
+            symbol: 'IBM',
+            buying_price: 80.0,
+            price: '...'
+          }
+        ],
     },
-    created : function () {
-        var fullDate = new Date();
-        this.date = fullDate.toDateString();
+    mounted: function () {
+        for (let stock of this.stocks) {
+            alpha_wrapper.fetch_price(stock);
+        }
     },
-    mounted : function () {
-        var alpha = alphavantage({ key: 'L7B1PRR8D9I68OSZ' });
-        alpha.data.daily('SQNS').then((data) => {
-            var daily = data["Time Series (Daily)"];
-            this.date = Object.keys(daily)[0];
-            var firstDay = daily[this.date];
-            this.message = "Open @" + firstDay["1. open"] + ". Close @" + firstDay["4. close"];
-        });
+    methods: {
+        sell_stock : function (stock) {
+            alert("Stock " + stock.symbol + " sold !");
+        }
     }
 });
