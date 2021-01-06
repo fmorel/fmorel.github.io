e8796a87de2ae38a3aa62f13f7ac8197c8fa9c0f
FX Morel <efix.morel@gmail.com>
2021-01-06 15:02:33 +0100

    Add charts !

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 Vue.config.productionTip = false;
 const price_wait = '...';
+const chart_size = 90;
 
 Vue.component('portfolio-element', {
     props: {
-        stock: Object
+        stock: Object,
     },
     computed: {
         get_price_style: function() {
@@ -45,7 +46,59 @@ Vue.component('portfolio-element', {
         <td> {{stock.amount}} </td>
         <td v-bind:style="get_price_style"> {{stock.price}} ({{get_percent}}%) </td>
         <td v-bind:style="get_price_style"> {{get_price}} </td>
-        <td class="centered"> <button class="pure-button" v-on:click="$emit('sell-stock')"> Sell </button> </td>
+        <td class="centered"> 
+            <button class="pure-button button-danger button-inline" v-on:click="$emit('sell-stock')"> Sell </button>
+            <button class="pure-button button-inline" v-on:click="$emit('toggle-chart')"> Chart </button>
+        </td>
+        </tr>
+    `
+});
+
+Vue.component('portfolio-chart', {
+    props: {
+        stock: Object,
+    },
+    mounted: function () {
+        var ctx = document.getElementById('chart_' + this.stock.id);
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels : this.stock.chart_label,
+                datasets : [{
+                    label: this.stock.symbol,
+                    data: this.stock.chart_data,
+                    borderColor : 'rgba(128, 128, 255, 0.8)',
+                    backgroundColor: 'rgba(64, 64, 192, 0.5)'
+                }]
+            },
+            options : {
+                scales : {
+                    xAxes : [{
+                        type: "time",
+                    }]
+                },
+                annotation: {
+                    annotations: [{
+                        type: 'line',
+                        mode: 'horizontal',
+                        display: true,
+                        scaleID: 'y-axis-0',
+                        value: this.stock.buying_price,
+                        borderColor: 'rgba(255, 128, 128, 0.8)',
+                        label: {
+                            enabled: true,
+                            content: 'Buying price'
+                        }
+                    }]
+                }
+            }
+        });
+    },
+    template : `
+        <tr>
+        <td colspan="6">
+        <canvas v-bind:id="'chart_' + stock.id"></canvas>
+        </td>
         </tr>
     `
 });
@@ -62,7 +115,10 @@ function AlphaWrapper2() {
         this.handle.timeSeries(this.options)
             .then(
                 (result) => {
-                    stock.price = result[0].high;
+                    var r = result.splice(0, chart_size);
+                    stock.price = r[0].high;
+                    stock.chart_data = r.map(x => x.high);
+                    stock.chart_label = r.map(x => x.date);
                 })
             .catch(
                 (error) => {
@@ -90,6 +146,9 @@ function Stock(id, symbol, buying_price, amount) {
     this.buying_price = buying_price;
     this.amount = amount;
     this.price = price_wait;
+    this.chart_data = [];
+    this.chart_label = [];
+    this.show_chart = false;
     this.error = false;
 }
 
@@ -114,7 +173,7 @@ var app = new Vue({
     mounted: function () {
         my_object = localStorage.getItem("stocks");
         if (my_object !== null)
-            this.stocks = JSON.parse(my_object).map(x => Object.setPrototypeOf(x, Stock.prototype));
+            this.stocks = JSON.parse(my_object).map(x => new Stock(x.id, x.symbol, x.buying_price, x.amount));
         this.currentId = this.stocks.reduce(
                             function(max, s) {
                                 if (s.id > max)
@@ -139,6 +198,9 @@ var app = new Vue({
             this.stocks.splice(this.stocks.indexOf(stock), 1);
             cash += stock.sold_price();
             this.refresh();
+        },
+        toggle_chart : function (stock) {
+            stock.show_chart = !stock.show_chart;
         },
         add_stock : function() {
             stock = new Stock(this.currentId,
