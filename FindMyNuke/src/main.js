ed8720f647163338362e1cda9bfac1bf81deb8c8
François-Xavier Morel <efix.morel@gmail.com>
2022-10-15 08:42:09 +0200

    Update main.js
    Fix syntax ?

--- FindMyNuke/src/main.js
+++ FindMyNuke/src/main.js
@@ -110,8 +110,8 @@
             /* Set image  and force it to 600px wide*/
             var dom_img = document.getElementById('picture_content');
             dom_img.src = img_url;
-            dom_img.max-width = "100%";
-            dom_img.height = "auto";
+            dom_img.width = "700px";
+            dom_img.height = 700*ratio +"px";
             display_clues(clues);
         }
     },
